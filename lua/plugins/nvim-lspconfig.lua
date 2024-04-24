56df642fa09739f99bd2b7dae734d3b76ce59551
Yunsen Lei <lei@yunsen.dev>
2024-04-24 18:09:48 -0500

    chnage the offset enconding configuration for clangd

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,5 +1,19 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        clangd = {
+          cmd = {
+            "clangd-12",
+            "--background-index",
+            "--clang-tidy",
+            "--header-insertion=iwyu",
+            "--completion-style=detailed",
+            "--offset-encoding=utf-16",
+          },
+        },
+      },
+    },
   },
 }
