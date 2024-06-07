85e953b82e3a0f66a0f89fa44bf670e4e6034e2d
Yunsen Lei <lei@yunsen.dev>
2024-06-06 22:52:39 -0400

    update lsp to disable auto-formatting

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,13 +2,13 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      autoformat = false,
       servers = {
         clangd = {
           cmd = {
             "clangd-12",
             "--background-index",
             "--clang-tidy",
-            "--header-insertion=iwyu",
             "--completion-style=detailed",
             "--offset-encoding=utf-16",
           },
