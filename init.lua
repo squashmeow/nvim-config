66ee1fe402bdaf559bce95917c74bb1e4087d9cf
Yunsen Lei <yunsen.lei@gmail.com>
2024-11-16 18:12:16 +0000

    update configuration for clangd

--- init.lua
+++ init.lua
@@ -1,10 +1,53 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 local lspconfig = require("lspconfig")
---- settings: the settings to pass to the language server (server-specific setting, instead of lsp client)
--- pyright settings reference: https://github.com/microsoft/pyright/blob/main/docs/settings.md
-lspconfig.pyright.setup({ settings = { python = { analysis = { typeCheckingMode = "basic" } } } })
 
-lspconfig.clangd.setup({ capabilities = { offsetEncoding = "utf-8" } })
+lspconfig.clangd.setup({
+  cmd = {
+    "/usr/bin/clangd-12",
+    "--background-index",
+    "--clang-tidy",
+    "--completion-style=detailed",
+    "--function-arg-placeholders",
+    "--fallback-style=llvm",
+  },
+  root_dir = function(fname)
+    return lspconfig.util.root_pattern(
+      "Makefile",
+      "configure.ac",
+      "configure.in",
+      "config.h.in",
+      "meson.build",
+      "meson_options.txt",
+      "build.ninja"
+    )(fname) or lspconfig.util.root_pattern("compile_commands.json", "compile_flags.txt")(fname) or lspconfig.util.find_git_ancestor(
+      fname
+    )
+  end,
+  capabilities = {
+    offsetEncoding = { "utf-16" },
+  },
+  init_options = {
+    usePlaceholders = true,
+    completeUnimported = true,
+    clangdFileStatus = true,
+  },
+  on_attach = function(client, bufnr)
+    -- Keymap for switching between source and header files
+    vim.api.nvim_buf_set_keymap(
+      bufnr,
+      "n",
+      "<leader>ch",
+      "<cmd>ClangdSwitchSourceHeader<cr>",
+      { noremap = true, silent = true, desc = "Switch Source/Header (C/C++)" }
+    )
+  end,
+})
 
-lspconfig.rust_analyzer.setup({})
+-- Optional: Handling clangd_extensions setup, if required
+local status_ok, clangd_ext = pcall(require, "clangd_extensions")
+if status_ok then
+  clangd_ext.setup({
+    server = lspconfig.clangd,
+  })
+end
